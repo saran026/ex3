2a3aa0b3786753185ac4829485b83b795a05b6be
saran026 <23203026@srcas.ac.in>
2024-07-01 11:57:51 +0530

    Create saranpranesh.java

---
+++ saranpranesh.java
@@ -0,0 +1,10 @@
+class saranpranesh{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello");
+      System.out.println("Welcome");
+      System.out.println("to");
+      System.out.println("Devops");
+      System.out.println("Class");
+  }
+                        
